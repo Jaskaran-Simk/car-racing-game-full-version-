ffa8132fa2d9e6c6efaefad792f94c3b2e01676a
jaskaran <rashankaur867@gmail.com>
2020-10-01 16:55:06 +0530

    hlo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 var ball;
+var database;
+var position;
+var pos;
+
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database()
+    position = database.ref("ball/position")
+    position.on("value",readPosition)
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
@@ -9,21 +16,30 @@ function setup(){
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    
+    database.ref("ball/position").set({
+        x: ball.x + x,
+        y: ball.y + y
+    })
+}
+
+function readPosition(data) {
+    pos = data.val()
+    ball.x = pos.x
+    ball.y = pos.y
 }
